7a74a7f71f126556adf3b1b30c4cd9ba3fdae134
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-04-24 13:41:44 -0400

    Beginning Leiningen integration

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject jblas "1.2.1"
+  :description "Linear Algebra for Java // Clojars version"
+  :url "https://github.com/tel/jblas"
+  :min-lein-version "2.0.0"
+  :source-paths [:native "src/main/c"]
+  :java-source-paths ["src/main/java"]
+  :resource-paths ["src/main/resource"]
+  :native-path "src/main/c"
+  :dependencies [[junit/junit "3.8.1"]])
