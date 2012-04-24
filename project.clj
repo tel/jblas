4d9ebbfc6a5967ca46a43f91e34daa9be9d25dca
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-04-24 14:42:03 -0400

    Updated native dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject jblas "1.2.1"
+(defproject jblas/jblas "1.2.1"
   :description "Linear Algebra for Java // Clojars version"
   :url "https://github.com/tel/jblas"
   :min-lein-version "2.0.0"
-  :source-paths [:native "src/main/c"]
+  :source-paths []
   :java-source-paths ["src/main/java"]
   :resource-paths ["src/main/resource"]
-  :native-path "src/main/c"
-  :dependencies [[junit/junit "3.8.1"]])
+  :dependencies [[jblas/native "1.2.0"]
+                 [junit/junit "3.8.1"]])
